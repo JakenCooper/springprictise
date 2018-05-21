7ebfd255a6d38ee66780c00cf607aa6a6dfaccdf
JakenCooper <jaken_cooper@hotmail.com>
2018-05-21 22:37:16 +0800

    base main version

---
+++ src/main/java/com/jaken/sp/config/RootConfig.java
@@ -0,0 +1,8 @@
+package com.jaken.sp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ src/main/java/com/jaken/sp/config/WebConfig.java
@@ -0,0 +1,55 @@
+package com.jaken.sp.config;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.jaken.sp.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter{
+	
+	@Bean(name="viewResolver")
+	public ViewResolver getDefaultViewResolver() {
+		InternalResourceViewResolver irv=new InternalResourceViewResolver();
+		irv.setPrefix("/WEB-INF/pages/");
+		irv.setSuffix(".jsp");
+		return irv;
+	}
+
+	
+	/**
+	 * 配置静态资源访问
+	 * */
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+	}
+
+//	@Override
+//	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//		registry.addResourceHandler(pathPatterns)
+//	}
+
+	
+	/**
+	 * 配置json访问解析
+	 * */
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		MappingJackson2HttpMessageConverter jsonconverter=new MappingJackson2HttpMessageConverter();
+		converters.add(jsonconverter);
+	}
+	
+	
+}
